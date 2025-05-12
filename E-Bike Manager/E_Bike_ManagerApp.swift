d267c1239855cbdc4f81c205446fe3d22d3568b4
Maxim Svidrak <151841989+MaximSvid@users.noreply.github.com>
2025-05-12 23:01:44 +0200

    Initial Commit

---
+++ E-Bike Manager/E_Bike_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_Bike_ManagerApp.swift
+//  E-Bike Manager
+//
+//  Created by Maxim Svidrak on 12.05.25.
+//
+
+import SwiftUI
+
+@main
+struct E_Bike_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
